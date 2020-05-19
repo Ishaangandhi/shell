379053cea009557ce74d0a4993f31e4a95d53c20
ishaangandhi <ishaangandhi@gmail.com>
2020-05-19 00:35:41 -0400

    new payments

---
+++ assets/js/payment.js
@@ -0,0 +1,19 @@
+function pay() {
+    // var PUBLISHABLE_KEY = "pk_test_FBbenQVpcDu1h5GNOoYx1Oh400VKt6pxFn"; // testing
+    var PUBLISHABLE_KEY = "pk_live_U9czz5WpQ4wDFAN38pghQp8g00SwwHnFiR"; // prod
+    var DOMAIN = "https://shellnotebook.com";
+    // var PRICE_ID = "price_HHircHAgfMBpCL"; // testing
+    var PRICE_ID = "price_HIyPH0qoaTCxUz"; // prod
+
+    var stripe = Stripe(PUBLISHABLE_KEY);
+    stripe
+        .redirectToCheckout({
+            mode: 'payment',
+            lineItems: [{ price: PRICE_ID, quantity: 1 }],
+            successUrl:
+                DOMAIN + "/success.html?session_id={CHECKOUT_SESSION_ID}",
+            cancelUrl: DOMAIN + ""
+        });
+}
+
+document.getElementById("buyButton").onclick = pay;
